0e2367bb64c7a2c7376ce387cd8c33fed5035e3d
horusreyesmartinez0718-cyber <horusreyesmartinez0718@gmail.com>
2026-03-03 10:00:55 -0500

    año.cpp

--- ejercicio 7/año.cpp
+++ ejercicio 7/año.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std; 
 
+int main () {
+    int año; 
+    bool bisiesto; 
+
+    cout << "Ingrese el año "; 
+    cin >> año; 
+
+    bisiesto = (año % 4 == 0 && año % 100 != 0) || (año % 400 == 0);
+
+    if(bisiesto)
+    {
+        cout << "El año " << año << " es bisiesto. ";
+    }
+    else 
+    {
+        cout << "El año " << año << " no es bisiesto. ";
+    }
+
+    return 0;
+}
