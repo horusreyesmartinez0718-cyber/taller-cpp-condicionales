0299da5e2dedd5de22a1c0e899e4db8f3280d952
horusreyesmartinez0718-cyber <horusreyesmartinez0718@gmail.com>
2026-03-03 10:28:42 -0500

    analizador.cpp

--- ejercicio 9/analizador.cpp
+++ ejercicio 9/analizador.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    double peso, altura, imc;
+
+    cout << "Ingrese su peso (kg): ";
+    cin >> peso;
+
+    cout << "Ingrese su altura (m): ";
+    cin >> altura;
+
+    imc = peso / (altura * altura);
+
+    cout << "Su IMC es: " << imc << endl;
+
+    if (imc < 18.5)
+        {
+            cout << "Clasificacion: Bajo peso";
+        }    
+    else if (imc < 25)
+    {
+        cout << "Clasificacion: Normal";
+    }
+    else if (imc < 30)
+    {
+        cout << "Clasificacion: Sobrepeso";
+    }
+    else if (imc < 35)
+    {
+        cout << "Clasificacion: Obesidad I";
+    }
+    else
+    {
+        cout << "Clasificacion: Obesidad II";
+    }
+
+    return 0;
+}
