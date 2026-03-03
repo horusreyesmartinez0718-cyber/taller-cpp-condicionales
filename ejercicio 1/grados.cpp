5908b64aefa136efee6ee95b513194982aeeff16
horusreyesmartinez0718-cyber <horusreyesmartinez0718@gmail.com>
2026-03-03 07:44:11 -0500

    grados.cpp

--- ejercicio 1/grados.cpp
+++ ejercicio 1/grados.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    double celsius;
+    double fahrenheit;
+    double kelvin;
+    double rankine;
+
+    cout << "Ingrese la temperatura en grados celcius: ";
+    cin >> celsius;
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+    kelvin = celsius + 237.15;
+    rankine = (celsius + 273.15) * 9 / 5;
+
+    cout << "Temperatura en Fahrenheit: " << fahrenheit << endl;
+    cout << "Temperatura en Kelvin: " << kelvin << endl; 
+    cout << "Temperatura en Rankine: " << rankine << endl; 
+    
+    return 0;
+}
