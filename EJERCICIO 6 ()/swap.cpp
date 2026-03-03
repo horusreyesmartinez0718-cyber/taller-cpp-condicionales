85d587c1f4ae6897e01195c7bb2da7d85dccbfb6
horusreyesmartinez0718-cyber <horusreyesmartinez0718@gmail.com>
2026-03-03 09:51:38 -0500

    swap.cpp

--- EJERCICIO 6 ()/swap.cpp
+++ EJERCICIO 6 ()/swap.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std; 
 
+int main () {
+    int a = 5; 
+    int b = 10; 
+
+    cout << "Valores: a =" << a << ", b = " << b << endl; 
+
+    int x = a; 
+    int y = b; 
+    int temporal = x; 
+    x = y; 
+    y = temporal; 
+    cout << "Metodo 1 (temporal): a = " << x << ", b = " << y << endl; 
+
+    int x = a; 
+    int y = b; 
+    x = x + y; 
+    y = x - y; 
+    x = x - y; 
+    cout << "Metodo 2 (aritmetico): a = " << x << ", b = " << y << endl; 
+
+    int x = a; 
+    int y = b; 
+    x = x + y; 
+    y = x - y; 
+    x = x - y; 
+    cout << "Metodo 3 (XOR): a =" << x << ", b = " << y << endl; 
+
+    return 0; 
+}
