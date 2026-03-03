1f1882750e0572fbffc68c749758bb338df511a1
horusreyesmartinez0718-cyber <horusreyesmartinez0718@gmail.com>
2026-03-03 09:16:39 -0500

    calificaciones.cpp

--- ejercicio 5/calificaciones.cpp
+++ ejercicio 5/calificaciones.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int nota;  
+
+    cout << "Ingrese la calificación: ";
+    cin >> nota;
+    
+    if(nota < 0 || nota > 50) {
+        cout << "ERROR";
+    }
+    
+    cout << "Calificacion: " << nota << endl; 
+
+    if (nota >= 30)
+    {
+        cout << "APROBADO"; 
+    }
+    else
+    {
+        cout << "REPROBADO"; 
+    }
+
+    return 0; 
+
+}
